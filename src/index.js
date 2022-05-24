c926fc8db197a558787b93354ed15bbee47de336
Devin Garcia <devingarcia5423@gmail.com>
2022-05-24 16:26:48 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
-let addToy = false;
+// let addToy = false;
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
+const baseURL = "http://localhost:3000/toys"
+const toyCollect = document.getElementById('toy-collection')
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
-});
+const getToys = () => {
+    fetch(baseURL)
+      .then((res) => res.json())
+      .then((arr) => arr.forEach((toy) => renderToys(toy)))
+}
+
+// function parseToys(arr) {
+//   arr.forEach(toy => {
+//     renderToys(toy)
+//     console.log(toy)
+//   });
+// }
+
+const renderToys = (toy) => {
+  let h2 = document.createElement('h2')
+  let img = document.createElement('img')
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  let card = document.createElement('div')
+
+  h2.textContent = toy.name
+  img.src = toy.image
+  img.className = 'toy-avatar'
+  p.textContent = `${toy.likes} likes`
+  btn.className = 'like-btn'
+  btn.textContent = 'like'
+  btn.id = toy.id
+  card.className = 'card'
+
+  card.append(h2, img, p, btn)
+  toyCollect.append(card)
+}
+
+getToys()
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   addBtn.addEventListener("click", () => {
+//     // hide & seek with the form
+//     addToy = !addToy;
+//     if (addToy) {
+//       toyFormContainer.style.display = "block";
+//     } else {
+//       toyFormContainer.style.display = "none";
+//     }
+//   });
+// });
